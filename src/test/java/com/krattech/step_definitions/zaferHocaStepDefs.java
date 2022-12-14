e56861cd8ef1035125368f08c384c0be46cc1508
HP <ihsanakcair@gmail.com>
2022-12-14 13:55:46 +0300

    test is established

---
+++ src/test/java/com/krattech/step_definitions/zaferHocaStepDefs.java
@@ -0,0 +1,63 @@
+package com.krattech.step_definitions;
+
+import com.krattech.pages.DashboardPage;
+import com.krattech.pages.IframePage;
+import com.krattech.pages.LoginPages;
+import com.krattech.utilities.ConfigurationReader;
+import com.krattech.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class zaferHocaStepDefs {
+    DashboardPage dashboardPage = new DashboardPage();
+
+    IframePage iframePage=new IframePage();
+
+    @Given("The user is on the Login Page")
+    public void the_user_is_on_the_login_page() {
+        Driver.get().get(ConfigurationReader.get("url"));
+    }
+
+    @When("The user input valid credentials to {string} and {string} bars")
+    public void the_user_input_valid_credentials_to_and_bars(String userEmail, String password) {
+        LoginPages loginPages = new LoginPages();
+        loginPages.loginWithParameter(userEmail, password);
+    }
+
+    @Then("The user verify that on the {string} page")
+    public void theUserVerifyThatOnThePage(String relatedPageTitle) {
+
+        String homeName = dashboardPage.getHomeName(relatedPageTitle);
+        Assert.assertEquals(relatedPageTitle, homeName);
+    }
+
+    @When("The user navigate to {string} {string}")
+    public void the_user_navigate_to(String tab, String module) {
+        dashboardPage.navigateToModule(tab, module);
+    }
+
+    @Then("The user verfiy that {string}")
+    public void the_user_verfiy_that(String moduleSubtitle) {
+        String actualSubtitle = dashboardPage.getSubtitle(moduleSubtitle);
+        Assert.assertEquals(moduleSubtitle,actualSubtitle);
+    }
+
+    @When("The user should close all tabs")
+    public void the_user_should_close_all_tabs() {
+
+        Driver.get().switchTo().frame(iframePage.iframe_loc);
+        iframePage.closeIframes();
+        Driver.get().switchTo().defaultContent();
+    }
+    @Then("The user verify that all tabs are closed")
+    public void the_user_verify_that_all_tabs_are_closed() {
+        Driver.get().navigate().back();
+        String actualURL = Driver.get().getCurrentUrl();
+        String expectedURL="https://www.krafttechexlab.com/index";
+        Assert.assertEquals(expectedURL, actualURL);
+    }
+
+
+}
